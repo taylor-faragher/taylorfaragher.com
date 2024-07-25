a260aecdf342317d1377a0eb6e11a4af87f67645
Taylor Faragher <faragher6@gmail.com>
2024-07-25 13:57:47 -0500

    CSS tweaks to the Resume section

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -6,8 +6,8 @@ import { BackgroundGradientAnimation } from "./ui/GradientBackground";
 
 const Resume = () => {
     return (
-        <section id="resume" className="pb-20 pt-10">
-            <div className="flex justify-center text-center min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap">
+        <section id="resume" className="pb-16 md:pb-0">
+            <div className="flex justify-center text-center min-w-full shrink-0 gap-16 py-4 flex-nowrap h-[50vh] md:h-[40vh] w-auto">
                     
                 <div className="w-[100vw] max-w-full relative rounded-2xl border flex-shrink-0 border-slate-800 md:w-[65vw] md:h-[32vh] overflow-hidden" data-test-id="testId">
                     <BackgroundGradientAnimation containerClassName="rounded-2xl h-[50vh]">
@@ -17,12 +17,12 @@ const Resume = () => {
                         <h3 className="heading leading-[1.6] text-white font-normal z-50 pb-14">
                             Download it here!
                         </h3>
-                        <a href='/Taylor Faraghers Resume.pdf' download='Taylor Faragher&#39;s Resume' className="z-50">
+                        <a href='/Taylor Faraghers Resume.pdf' download='Taylor Faragher&#39;s Resume' className="z-50 ">
                             <MagicButton 
                                 title="Taylor Faragher's Resume.pdf"
                                 icon=""
                                 position=""
-                                buttonClasses="z-50 h-20 md:w-96"
+                                buttonClasses="z-50 h-20 md:w-96 max-w-[90%]"
                                 otherClasses="text-xl"
                             />
                         </a>
