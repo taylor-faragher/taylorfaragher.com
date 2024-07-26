326f9930aa3a0668e333b530d0383aad1d02f506
Taylor Faragher <faragher6@gmail.com>
2024-07-26 14:31:59 -0500

    Updates link verbage on recent projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -36,7 +36,7 @@ const RecentProjects = () => {
                                 </div>
                                 
                                 <div className="flex justify-center items-center">
-                                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">Check Live Site</p>
+                                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">See on Github</p>
                                     <FaLocationArrow className="ms-3" color="#CBACF9" />
                                 </div>
                             </div>            
