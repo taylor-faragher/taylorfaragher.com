99c640d76e229f789aff2e2cf2f2efd676694bf7
Taylor Faragher <faragher6@gmail.com>
2024-07-25 23:26:29 -0500

    Adds vercel speed insights utility

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,12 @@ import Resume from "../components/Resume";
 import Experience from "@/components/Experience";
 import Process from "@/components/Process";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center overflow-hidden flex-col mx-auto sm:px-10 px-5">
+      <SpeedInsights/>
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero />
