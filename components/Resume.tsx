0f6a23940a4189373dd555f4816bce4ba0f52d83
Taylor Faragher <faragher6@gmail.com>
2025-01-25 23:25:47 -0600

    Adjusts spacing around resume button

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -9,7 +9,7 @@ const Resume = () => {
         <section id="resume" className="pb-16 md:pb-0">
             <div className="flex justify-center text-center min-w-full shrink-0 gap-16 py-4 flex-nowrap h-[45vh] lg:h-[35vh] xl:h-[35vh] w-auto">
                     
-                <div className="w-[100vw] max-w-full sm:max-w-[85%] md:max-w-[80%] relative rounded-2xl border flex-shrink-0 border-slate-800 h-auto md:w-[70vw] overflow-hidden" data-test-id="testId">
+                <div className="w-[100vw] max-w-full sm:max-w-[85%] md:max-w-[80%] relative rounded-2xl border flex-shrink-0 border-slate-800 h-[336px] md:w-[70vw] overflow-hidden" data-test-id="testId">
                     <BackgroundGradientAnimation containerClassName="rounded-2xl h-[50vh]">
                         <h3 className="heading leading-[1.6] text-white font-normal z-50 pt-10 pb-5">
                             Looking for my latest resume?
